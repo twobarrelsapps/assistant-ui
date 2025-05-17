6f5f864eaaaace116c0ec06e233affb5dfbb6f97
Simon Farshid <simon@assistant-ui.com>
2025-05-16 17:33:24 -0700

    fix: build (#1943)

--- packages/react/src/model-context/tool.ts
+++ packages/react/src/model-context/tool.ts
@@ -1,5 +1,7 @@
 import { Tool } from "assistant-stream";
 
+// TODO re-add the inferrence of the parameters
+
 export function tool<TArgs extends Record<string, unknown>, TResult = any>(
   tool: Tool<TArgs, TResult>,
 ): Tool<TArgs, TResult> {
